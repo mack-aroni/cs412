6ec76219815eb0b0d65c75fd2ff0e1ca65307427
mack-aroni <etanm2013@gmail.com>
2025-02-21 17:15:43 -0500

    minor changes

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import ShowAllView, ShowProfilePageView #, RandomArticleView
+from .views import ShowAllView, ShowProfilePageView
 
 urlpatterns = [
     path('', ShowAllView.as_view(), name="show_all_profiles"),
-    # path('show_all', ShowAllView.as_view(), name="show_all"), # modified
     path('profile/<int:pk>', ShowProfilePageView.as_view(), name='show_profile'),
 ]
