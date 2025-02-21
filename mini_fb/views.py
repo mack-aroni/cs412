1fe962f83ded388019701abcc888e2a307a2744d
mack-aroni <etanm2013@gmail.com>
2025-02-21 17:23:45 -0500

    minor change

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -3,7 +3,6 @@ from django.views.generic import ListView, DetailView
 
 class ShowAllView(ListView):
     '''Create a subclass of ListView to display all blog profiles.'''
-
     model = Profile # retrieve objects of type Profile from the database
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles' # how to find the data in the template file
