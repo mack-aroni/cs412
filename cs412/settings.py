334b0f3e0b32f16af543da4002425b8b20d93de1
mack-aroni <etanm2013@gmail.com>
2025-03-07 16:07:53 -0500

    minor fix

--- cs412/settings.py
+++ cs412/settings.py
@@ -137,6 +137,7 @@ CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
 
 if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
     STATIC_URL = '/emach/static/'
+    MEDIA_URL = '/emach/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
