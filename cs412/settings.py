82d2764862c76e68861f42bf6e0728be8e4bc669
mack-aroni <etanm2013@gmail.com>
2025-04-02 18:23:21 -0400

    added marathon_analytics

--- cs412/settings.py
+++ cs412/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'restaurant',
     'blog',
     'mini_fb',
+    'marathon_analytics',
 ]
 
 MIDDLEWARE = [
---
+++ marathon_analytics/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    # map the URL (empty string) to the view
+    path('', views.ResultsListView.as_view(), name='home'),
+    path('results', views.ResultsListView.as_view(), name='results_list'),
+    path('result/<int:pk>', views.ResultDetailView.as_view(), name='result_detail'),
+
+]
