5d234f48beb89c23f280706664feb88c150f4665
mack-aroni <etanm2013@gmail.com>
2025-04-03 10:17:15 -0400

    first submit

--- voter_analytics/views.py
+++ voter_analytics/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+
+class ResultView(View):
+    '''View to display voter results'''
+
+    template_name = 'voter_analytics/base.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
